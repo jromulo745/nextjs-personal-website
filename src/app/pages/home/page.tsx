dbf0ade920aaaa598d58a255271d26861d21bf10
jromulo745 <jromulo745@gmail.com>
2024-07-04 00:20:32 -0700

    fix typo

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
         <div className="backdrop-blur-md border rounded-3xl m-20 p-10 max-w-3xl">
           <h1 className="text-4xl mb-5">Joshua Jefson Romulo</h1>
           <h2 className="text-3xl mb-5">IT | Software Developer</h2>
-          <p className="mt-5" style={{color: '#6E717A'}}>Hello! My name is Joshua Jefson Romulo, d this is my personal website, programmed from the ground up using NextJS. I'm a computer science graduate with work experience in an IT setting, and some of my interests include web development, creating & producing music, and video games. Cheers!</p>
+          <p className="mt-5" style={{color: '#6E717A'}}>Hello! My name is Joshua Jefson Romulo, and this is my personal website, programmed from the ground up using NextJS. I'm a computer science graduate with work experience in an IT setting, and some of my interests include web development, creating & producing music, and video games. Cheers!</p>
         </div>
       </div>
     </div>
