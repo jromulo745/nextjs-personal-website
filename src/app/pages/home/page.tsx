01e4df6e4c57b6532a3c60a5356f955949700c2d
jromulo745 <85465170+jromulo745@users.noreply.github.com>
2024-07-07 23:45:34 -0700

    update format of the home page boxes

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -17,24 +17,24 @@ export default function HomePage() {
       <div style={{maxWidth: '768px', margin: '0 auto', padding: '8px 0 0 0'}}>
         <div className="flex mt-5 mb-5" style={{justifyContent: 'around', gap: '15px'}}>
           
-          <div className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 shrink min-w-0 min-h-0 overflow-hidden flex flex-col" style={{alignItems: 'center', flex: '1'}}>
+          <div className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 min-w-0 min-h-0 overflow-hidden flex flex-col" style={{alignItems: 'center', flex: '1'}}>
             <h2 className="flex justify-center">About Me</h2>
-            <img style={{marginTop: '25px', marginBottom: 'auto'}} src="/images/photo.png" width="127" height="169" className="mb-2 flex m-auto border rounded-xl"></img>
+            <img src="/images/photo.png" width="127" height="169" className="border rounded-xl" style={{marginTop: '10px', marginBottom: '10px'}}></img>
             <p>Click this box to view more information about me</p>
           </div>
    
-          <div className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 shrink min-w-0 min-h-0 overflow-hidden flex flex-col" style={{alignItems: 'center', flex: '1'}}>
+          <div className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 min-w-0 min-h-0 overflow-hidden flex flex-col" style={{alignItems: 'center', flex: '1'}}>
             <h2 className="flex justify-center">GitHub</h2>
-            <img style={{marginTop: '25px', marginBottom: 'auto'}} src="/images/github-mark.png" width="159" height="162" className="mb-2 flex m-auto"></img>
+            <img src="/images/github-mark.png" width="159" height="162" style={{marginTop: '10px', marginBottom: '20px'}}></img>
             <p>Check out my GitHub!</p>
           </div>
           
-          <div className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 shrink min-w-0 min-h-0 overflow-hidden flex flex-col" style={{alignItems: 'center', flex: '1'}}>
+          <div className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 min-w-0 min-h-0 overflow-hidden flex flex-col" style={{alignItems: 'center', flex: '1'}}>
             <h2 className="flex justify-center">LinkedIn</h2>
-            <img style={{marginTop: '25x', marginBottom: 'auto'}} src="/images/LI-In-Bug.png" width="201" height="170" className="mb-2 flex m-auto"></img>
+            <img  src="/images/LI-In-Bug.png" width="201" height="170" style={{marginTop: '25px', marginBottom: '25px'}}></img>
             <p>Connect with me on LinkedIn!</p>
           </div>
-    
+  
         </div>
       </div>
       {/* ------------------------------------------------------------------- */}
