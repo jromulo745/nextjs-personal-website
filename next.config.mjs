8b7cbfbd11a6b2b17b6c8b9cb0326e7e3484759c
jromulo745 <85465170+jromulo745@users.noreply.github.com>
2024-07-14 21:10:15 -0700

    ignore eslint during build production

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false // disable Strict Mode to disable double rendering
+  reactStrictMode: false, // disable Strict Mode to disable double rendering
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
