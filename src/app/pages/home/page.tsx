7d782415157bc2afa7ab6bd1dc8d99a0431a43b2
jromulo745 <jromulo745@gmail.com>
2024-07-20 23:24:20 -0700

    work on responsive web design for mobile devices for the home page

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <Navbar />
       {/* ------------------------------------------------ */}
       <div className="flex justify-center">
-        <div className="backdrop-blur-md border shadow-lg rounded-3xl ml-20 mr-20 mt-10 p-10 max-w-3xl">
+        <div className="backdrop-blur-md border shadow-lg rounded-3xl ml-20 mr-20 mt-10 p-10 max-w-3xl introduction">
           <h2 className="flex justify-center text-3xl mb-5" style={{color: '#9B9EAE'}}>IT | Software Developer</h2>
           <p className="mt-5" style={{color: '#6E717A'}}>Hello! My name is Joshua Jefson Romulo, and this is my portfolio website, programmed  using NextJS. I'm a computer science graduate with work experience in IT, and some of my interests include web development, music production, and film appreciation. Cheers!</p>
         </div>
@@ -26,13 +26,13 @@ export default function HomePage() {
             <p>Click here to view more information about me</p>
           </Link>
    
-          <Link href="https://github.com/jromulo745" target="_blank" className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 min-w-0 min-h-0 overflow-hidden flex flex-col hover:shadow-2xl" style={{alignItems: 'center', flex: '1'}}>
+          <Link href="https://github.com/jromulo745" target="_blank" className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 min-w-0 min-h-0 overflow-hidden flex flex-col hover:shadow-2xl box2" style={{alignItems: 'center', flex: '1'}}>
             <h2 className="flex justify-center">GitHub</h2>
             <img src="/images/github-mark.png" width="159" height="162" style={{marginTop: '10px', marginBottom: '20px'}}></img>
             <p>Check out my GitHub!</p>
           </Link>
           
-          <Link href="https://www.linkedin.com/in/joshuaromulo/" target="_blank" className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 min-w-0 min-h-0 overflow-hidden flex flex-col hover:shadow-2xl" style={{alignItems: 'center', flex: '1'}}>
+          <Link href="https://www.linkedin.com/in/joshuaromulo/" target="_blank" className="backdrop-blur-md border shadow-lg rounded-xl pl-10 pr-10 pt-5 pb-5 min-w-0 min-h-0 overflow-hidden flex flex-col hover:shadow-2xl box3" style={{alignItems: 'center', flex: '1'}}>
             <h2 className="flex justify-center">LinkedIn</h2>
             <img  src="/images/LI-In-Bug.png" width="201" height="170" style={{marginTop: '25px', marginBottom: '25px'}}></img>
             <p>Connect with me on LinkedIn!</p>
