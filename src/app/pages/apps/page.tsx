9d981d11b9c83647d7214be246d1f0015d698034
jromulo745 <jromulo745@gmail.com>
2024-09-23 20:35:09 -0700

    create a testing section for THM, and add initial testing files and data

--- src/app/pages/apps/page.tsx
+++ src/app/pages/apps/page.tsx
@@ -18,14 +18,21 @@ export default function AppPage() {
       </div>
       {/* ----------------------------------------------------------------- */}
       <div className="flex justify-center">
-        <div className="backdrop-blur-md border shadow-lg rounded-3xl ml-20 mr-20 mb-20 p-10 max-w-3xl eleven-o-two">
+        <div className="backdrop-blur-md border shadow-lg rounded-3xl ml-20 mr-20 mb-10 p-10 max-w-3xl eleven-o-two">
           <h1 className="text-3xl" style={{color: 'grey'}}>CompTIA Testing (220-1102)</h1>
           <p className="mt-5 mb-5">Click the start button to open a cluster of testing components used as a study aid to prepare for the CompTIA 220-1102 certification exam.</p>
           <Link className="border rounded-2xl p-2.5 hover:shadow-lg" style={{color: '#6E717A', borderColor: 'grey'}} href="/pages/apps/220-1102">Start</Link>
         </div>
       </div>
       {/* ----------------------------------------------------------------- */}
-      
+      <div className="flex justify-center">
+        <div className="backdrop-blur-md border shadow-lg rounded-3xl ml-20 mr-20 mb-20 p-10 max-w-3xl eleven-o-two">
+          <h1 className="text-3xl" style={{color: 'grey'}}>THM Testing</h1>
+          <p className="mt-5 mb-5">Click the start button to open a cluster of testing components used as a study aid to learn more about cybersecurity.</p>
+          <Link className="border rounded-2xl p-2.5 hover:shadow-lg" style={{color: '#6E717A', borderColor: 'grey'}} href="/pages/apps/thm">Start</Link>
+        </div>
+      </div>
+      {/* ----------------------------------------------------------------- */}
     </div>
   );
 }
