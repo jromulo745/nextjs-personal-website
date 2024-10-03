6340669aba3522b928385ef971b72e0c2fbfcd4a
jromulo745 <jromulo745@gmail.com>
2024-10-02 23:18:07 -0700

    begin the site section for N10-009

---
+++ src/app/pages/apps/N10-009/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import Navbar from "../../../components/navbar";
+import MultipleChoice from '../../../components/multiple-choice'
+import FillIn from "../../../components/fill-in";
+import ExamAlerts from "../../../components/exam_alerts"
+
+export default function Page() {
+  return (
+    <div>
+      <Navbar />
+      {/* ---------------------------------------------------------- */}
+      <MultipleChoice file_path_1={"/N10-009/multiple-choice.json"} file_path_2={"/220-1102/multiple-choice-explanations.json"} />
+      {/* ---------------------------------------------------------- */}
+      <FillIn file_path={"/N10-009/fill-in.json"} />
+      {/* ---------------------------------------------------------- */}
+      <ExamAlerts file_path={"/N10-009/exam_alerts.json"}></ExamAlerts>
+      <br></br>
+      <br></br>
+      <br></br>
+    </div>
+  );
+}
+
+// ---------------------------------------------------------------------
--- src/app/pages/apps/page.tsx
+++ src/app/pages/apps/page.tsx
@@ -12,7 +12,7 @@ export default function AppPage() {
       <div className="flex justify-center">
         <div className="backdrop-blur-md border shadow-lg rounded-3xl ml-20 mr-20 mb-10 mt-10 p-10 max-w-3xl eleven-o-one">
           <h1 className="text-3xl" style={{color: 'grey'}}>CompTIA Testing (220-1101)</h1>
-          <p className="mt-5 mb-5">Click the start button to open a cluster of testing components used as a study aid to prepare for the CompTIA 220-1101 certification exam.</p>
+          <p className="mt-5 mb-5">Click the start button to open a cluster of testing components used as a study aid to prepare for the CompTIA A+ 220-1101 certification exam.</p>
           <Link className="border rounded-2xl p-2.5 hover:shadow-lg" style={{color: '#6E717A', borderColor: 'grey'}} href="/pages/apps/220-1101">Start</Link>
         </div>
       </div>
@@ -20,8 +20,16 @@ export default function AppPage() {
       <div className="flex justify-center">
         <div className="backdrop-blur-md border shadow-lg rounded-3xl ml-20 mr-20 mb-10 p-10 max-w-3xl eleven-o-two">
           <h1 className="text-3xl" style={{color: 'grey'}}>CompTIA Testing (220-1102)</h1>
-          <p className="mt-5 mb-5">Click the start button to open a cluster of testing components used as a study aid to prepare for the CompTIA 220-1102 certification exam.</p>
+          <p className="mt-5 mb-5">Click the start button to open a cluster of testing components used as a study aid to prepare for the CompTIA A+ 220-1102 certification exam.</p>
           <Link className="border rounded-2xl p-2.5 hover:shadow-lg" style={{color: '#6E717A', borderColor: 'grey'}} href="/pages/apps/220-1102">Start</Link>
+        </div>
+      </div>
+      {/* ----------------------------------------------------------------- */}
+      <div className="flex justify-center">
+        <div className="backdrop-blur-md border shadow-lg rounded-3xl ml-20 mr-20 mb-10 p-10 max-w-3xl eleven-o-two">
+          <h1 className="text-3xl" style={{color: 'grey'}}>CompTIA Testing (N10-009)</h1>
+          <p className="mt-5 mb-5">Click the start button to open a cluster of testing components used as a study aid to prepare for the CompTIA Network+ N10-009 certification exam.</p>
+          <Link className="border rounded-2xl p-2.5 hover:shadow-lg" style={{color: '#6E717A', borderColor: 'grey'}} href="/pages/apps/N10-009">Start</Link>
         </div>
       </div>
       {/* ----------------------------------------------------------------- */}
