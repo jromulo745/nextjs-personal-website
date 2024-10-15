00ce32508989dac0112577b1220135abc8116f18
jromulo745 <jromulo745@gmail.com>
2024-10-14 19:00:16 -0700

    fix punctuation

--- src/app/components/fill-in.tsx
+++ src/app/components/fill-in.tsx
@@ -65,7 +65,7 @@ export default function FillIn({file_path}: {file_path: string}) {
       setTimeout(() => alert("Correct"), 10);
     } else {
       (document.getElementById("answer") as HTMLInputElement).style.backgroundColor = 'red';
-      setTimeout(() => alert("Not quite, the correct answer is \"" + answers[counter] + "\"\nYou entered \"" + answer + "\" dumbass"), 10);
+      setTimeout(() => alert("Not quite, the correct answer is \"" + answers[counter] + "\"\nYou entered \"" + answer + "\" dumbass."), 10);
       console.log('correct answer: ' + answers[counter]);
     }
 
