abba522f3ec5e78a209406ba6e6a23ca134fba36
Joshua Romulo <joshuaromulo@Joshuas-MacBook-Pro.local>
2025-07-20 17:31:12 -0700

    remove unused imports and comments from the newly created port quiz page

--- src/app/pages/apps/port-quiz/page.tsx
+++ src/app/pages/apps/port-quiz/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import Navbar from "../../../components/navbar";
-import MultipleChoice from '../../../components/multiple-choice'
 import FillIn from "../../../components/fill-in";
-import ExamAlerts from "../../../components/exam_alerts"
 
 export default function Page() {
   return (
@@ -16,5 +14,3 @@ export default function Page() {
     </div>
   );
 }
-
-// ---------------------------------------------------------------------
