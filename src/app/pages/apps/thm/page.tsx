3313de7521978e3439be682d8d622717d86f294a
Joshua Romulo <jromulo745@gmail.com>
2025-09-27 00:58:15 -0700

    configure thm and add questions to multiple choice

--- src/app/pages/apps/thm/page.tsx
+++ src/app/pages/apps/thm/page.tsx
@@ -10,8 +10,9 @@ export default function Page() {
     <div>
       <Navbar />
       {/* ---------------------------------------------------------- */}
-      <FillIn file_path={"/thm/fill-in.json"} />
+      <MultipleChoice file_path_1={"/thm/multiple-choice.json"} file_path_2={"/thm/multiple-choice-explanations.json"} />
       {/* ---------------------------------------------------------- */}
+      <FillIn file_path={"/thm/fill-in.json"} />
       <br></br>
       <br></br>
       <br></br>
